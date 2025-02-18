3264c7a0614910a92d6c28ecba4637ad72479a3c
e1kan <156985217+e1kan@users.noreply.github.com>
2025-02-18 14:52:59 +0500

    Delete Lab5/kaspi-app-redefined/src directory

--- Lab5/kaspi-app-redefined/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { ProductListComponent } from './products/product-list/product-list.component';
-
-export const appRoutes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'category/:id', component: ProductListComponent },
-];
